32b0bcc5e039ce3e856c3d2f8c7fde3075148220
Rinat Kamaletdinov <sky2208@ya.ru>
2021-04-06 00:04:06 +0400

    Fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-import read from "./read";
-import json from "./json";
-//import { reject, resolve } from "core-js/fn/promise";
+import read from './read';
+import json from './json';
+// import { reject, resolve } from "core-js/fn/promise";
+import GameSaving from './GameSaving';
+
 export default class GameSavingLoader {
-    constructor(){
-    }
-
-    load(){
-        (async () => {
-            try{
-                const data = await read();
-                const obj = await json(data);
-                for(let key in JSON.parse(obj)){
-                    this[key] = JSON.parse(obj)[key];
-                }
-            } catch(err){
-                throw err;
-            }
-
-        })();
-    }
+  static load() {
+    return (async () => {
+      try {
+        const data = await read();
+        const obj = await json(data);
+        return new GameSaving(JSON.parse(obj));
+      } catch (err) {
+        throw err;
+      }
+    })();
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,16 @@
-import GameSavingLoader from "./index";
-import json from "./json";
-import read from "./read";
+import GameSavingLoader from './index';
+import json from './json';
+import read from './read';
 
-test('asyn/await', ()=>{(async ()=> {
-    let obj = new GameSavingLoader();
-    let data = await obj.load();   
-        expect(data).toEqual({"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}});
-           
-})()})
-
-
-
-
-
+test('asyn/await', () => {
+  (async () => {
+    GameSavingLoader.load();
+    expect(GameSavingLoader.load()).toEqual({
+      id: 9,
+      created: 1546300800,
+      userInfo: {
+        id: 1, name: 'Hitman', level: 10, points: 2000,
+      },
+    });
+  })();
+});
